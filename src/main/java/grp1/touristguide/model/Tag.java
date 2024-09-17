ced9d24740eac6a63e6f1b65ed689e95ba94327e
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-09-17 09:18:44 +0100

    updated tag and controller to fix an issue displaying tags

--- src/main/java/grp1/touristguide/model/Tag.java
+++ src/main/java/grp1/touristguide/model/Tag.java
@@ -1,9 +1,20 @@
 package grp1.touristguide.model;
 
 public enum Tag {
-    CHILDFRIENDLY,
-    FREE,
-    ART,
-    MUSEUM,
-    NATURE
+    HISTORIC("Historic"),
+    FAMILY_FRIENDLY("Family-Friendly"),
+    NATURE("Nature"),
+    ADVENTURE("Adventure"),
+    CULTURAL("Cultural"),
+    RELAXING("Relaxing");
+
+    private final String displayName;
+
+    Tag(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
