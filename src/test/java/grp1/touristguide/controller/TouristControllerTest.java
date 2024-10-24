3088933d3cc3b5bca46d21df0ec2d1ab9b4d639c
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2024-10-24 14:34:56 +0100

    Removed H2 support

--- src/test/java/grp1/touristguide/controller/TouristControllerTest.java
+++ src/test/java/grp1/touristguide/controller/TouristControllerTest.java
@@ -24,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(controllers = grp1.touristguide.controller.TouristController.class)
 class TouristControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
